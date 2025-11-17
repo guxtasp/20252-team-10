6258d1a5ed4a5db8fa277367ac4cea9e3aeeb927
rrfietto <rrfietto@gmail.com>
2025-11-17 14:21:47 -0300

    primeira versao Makefile e script de setup

---
+++ resources/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Instalando dependências..."
+
+mkdir temp
+
+cd temp
+
+sudo wget https://dev.mysql.com/get/mysql-apt-config_0.8.36-1_all.deb
+
+sudo dpkg -i mysql-apt-config_0.8.36-1_all.deb
+
+sudo apt-get update
+
+sudo apt install libmysqlcppconn-dev
+
+cd ..
+
+rm -rf temp
+
+echo "Fim"
